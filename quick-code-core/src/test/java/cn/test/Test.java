1e9905d4a22e045058599063f2b54c2cac48f977
aiyongfeng <aiyongfeng@bonc.com.cn>
2021-02-24 12:19:09 +0800

    Test 1.0

---
+++ quick-code-core/src/test/java/cn/test/Test.java
@@ -0,0 +1,15 @@
+package cn.test;
+
+/**
+ * ClassName:Test
+ * Package:cn.test
+ * Description:
+ *
+ * @Author:aiyongfeng
+ * @Date:2021/2/24 12:18
+ */
+public class Test {
+	public static void main(String[] args) {
+
+	}
+}
